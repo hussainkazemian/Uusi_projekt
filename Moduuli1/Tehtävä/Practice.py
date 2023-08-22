ca37221855154865c3052f78039269a9d4355da7
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-08-22 15:37:25 +0200

    math yhtalot

---
+++ Moduuli1/Tehtävä/Practice.py
@@ -0,0 +1,25 @@
+#lisaa
+x = int(input("kirjoitta ensi numero: "))
+y = int(input("kirjoitta toinen numero: "))
+total = str(x + y)
+print("summa: " + total)
+print()
+#vahennyslasku
+x = int(input("kirjoitta ensi numero: "))
+y = int(input("kirjoitta toinen numero: "))
+total = str(x - y)
+print("vahennyslasku: " + total)
+print()
+#kertolasku
+x = int(input("kirjoitta ensi numero: "))
+y = int(input("kirjoitta toinen numero: "))
+total = str(x * y)
+print("kertolasku: " + total)
+print()
+#jako
+x = int(input("kirjoitta ensi numero: "))
+y = int(input("kirjoitta toinen numero: "))
+total = str(x / y)
+print("jako: " + total)
+print()
+
