cecbd3bc1ccb66ff704f66c479e045c290509c5a
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-08-22 10:41:10 +0200

    uusi info

--- Moduuli1/moduuli.py
+++ Moduuli1/moduuli.py
@@ -1,1 +1,15 @@
-print("Moikka Hussain")
+print("Hei Hussain\n,kazemian")
+print("miten menee?")
+name = input("kuka sä oot?")
+age = 22 + 5
+print(name)
+print (age)
+age = "12" + "5"
+print(age)
+age = str(age)
+print(name + "age: " + str(age))
+name = "Hussain"
+name = input("Kirjoittaa nimisi?")
+print (name)
+name = "Hussain" + "kazemian"
+print (name)
