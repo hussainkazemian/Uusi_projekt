dd78d2ad0420ac2b6e111ff55b38d7131807b1e3
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-08-22 15:37:25 +0200

    lisatieto tehtava2

---
+++ Moduuli1/Moduuli2/tehtava5.py
@@ -0,0 +1,12 @@
+leivi = int(input("Anna leiviskat:"))
+print(leivi)
+naula = int(input("Anna naulat:"))
+print(naula)
+luoti = int(input("Anna luodit:"))
+print(luoti)
+naulat = leivi * 20 + naula
+print(naulat)
+luotia = naula * 32 + luoti
+print(luotia)
+grammaa = luoti * 13.3
+print(grammaa)
