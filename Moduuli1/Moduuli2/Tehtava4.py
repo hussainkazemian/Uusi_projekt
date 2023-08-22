c5720ceb446b847fe26a3703f25cdfd01587207c
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-08-22 14:16:51 +0200

    lisatieto tehtava2

---
+++ Moduuli1/Moduuli2/Tehtava4.py
@@ -0,0 +1,12 @@
+kayttaja1 = int(input("kirjoitta numero:"))
+print(kayttaja1)
+kayttaja2 =int(input("kirjoitta numero:"))
+print(kayttaja2)
+kayttaja3 = int(input("kirjoitta numero:"))
+print(kayttaja3)
+kokonaisluka= kayttaja1 + kayttaja2 + kayttaja3
+print(kokonaisluka)
+keskiarvon= (kayttaja1 + kayttaja2 + kayttaja3) / 3
+print(keskiarvon)
+tulo= kayttaja1 * kayttaja2 * kayttaja3
+print(tulo)
