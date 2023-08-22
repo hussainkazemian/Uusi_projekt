724babda45865f930ae7907381e8f0e7e39d1021
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-08-22 14:57:38 +0200

    lisatieto tehtava2

--- Moduuli1/Tehtävä/harjotustehtävä.py
+++ Moduuli1/Tehtävä/harjotustehtävä.py
@@ -1,4 +1,6 @@
-print("Terve! hussain")
-name = input("kuka olet? ")
-print(f"Hello, {name}")
-
+width= int(input("How much is the width?"))
+height= int(input("How much is the height?"))
+area= width*height
+piiri = width *2 + height *2
+print(piiri)
+print(area)
