f4bdb2b9a4d05aef56a33ac08ef7001a94a605c7
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-09-03 19:31:20 +0200

    Refresh

--- Moduuli1/Tehtävät-ifin/If-tehtävä3.py
+++ Moduuli1/Tehtävät-ifin/If-tehtävä3.py
@@ -2,7 +2,7 @@ print("kirjoittaa M, jos olet miehen")
 print("kirjoittaa N, jos olet nainen")
 print("kirjoittaa R, jos et ole M ja N")
 käyttäjä = str(input("Anna sinun biologisen sukupuolen: ?"))
-hl = float(input("Anna sinun hemogolobiinarvo: ?"))
+hl = int(input("Anna sinun hemogolobiinarvo: ?"))
 if käyttäjä and hl < 134:
     print("käyttäjä on: Mies ja hemogolobiinoarvo on alhainen")
 elif käyttäjä and hl in range (134, 195):
@@ -15,7 +15,5 @@ if käyttäjä == "N" and hl in range(117, 175):
     print("käyttäjä on Naiset ja hemogolobiiniarvo on normaali")
 elif käyttäjä and hl >175:
     print("käyttäjä on naiset ja hemogolobiiniarvo on korkea")
-else:
-    print ("Anna uudellen oikein sukupuoli")
 
 
---
+++ Moduuli1/Tehtävät-ifin/if-tehtävä4.py
@@ -0,0 +1,5 @@
+vuosi = int(input("kirjoitta vuosi:"))
+if (vuosi % 4 ==0 and vuosi % 100!=0 or vuosi % 400==0):
+    print("vuosi on kakausvuosi")
+else:
+    print(" se ei ole kakausvuosi")
