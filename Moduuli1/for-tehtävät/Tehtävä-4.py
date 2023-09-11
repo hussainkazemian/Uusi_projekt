e1309d267040e0d0a565ac3552d0c7dad0674a8b
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-09-11 14:35:28 +0200

    Refresh

--- Moduuli1/for-tehtävät/Tehtävä-4.py
+++ Moduuli1/for-tehtävät/Tehtävä-4.py
@@ -1,14 +1,16 @@
-kaupungit = []
+
+"""kaupungit = []
 käyttäjä = input("kirjoitta kaupungin nimet: ")
 while käyttäjä!="":
     kaupungit.append(käyttäjä)
     käyttäjä = input("kirjoitta kaupungit nimet: ")
 for i in [kaupungit]:
     print(f"{kaupungit} ovat kaupungit nimet")
+"""
 #toinen vaihteehto
-    kaupungit = []
-    for i in range(5):
+kaupungit = []
+for i in range(5):
         nimi = input("Anna kaupungit nimi: ")
         kaupungit.append(nimi)
-    for kaupungit in kaupungit:
+for kaupungit in kaupungit:
         print(kaupungit)
